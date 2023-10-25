61220b65cf86f6d2624633a68a6d3d6f9701285b
konosubakonoakua <ailike_meow@qq.com>
2023-10-26 04:45:35 +0800

    chore: update qemuinstall script with deps

--- qemuinstall.sh
+++ qemuinstall.sh
@@ -1,2 +1,6 @@
 [ ! -f qemu.tar.xz ] && wget https://github.com/konosubakonoakua/imx6ul_qemu/releases/download/qemu_v1/qemu.tar.xz
 [ ! -d qemu ] && tar -Jxvf qemu.tar.xz -C . && rm -f qemu.tar.xz
+
+deps=(libsdl2-dev)
+dpkg -s ${deps[*]} >/dev/null
+[[ ! $? -eq 0 ]] && sudo apt install ${deps[*]}
